e2b6d3ea1a0b999e12474a3e21679315de663715
Trapland <89914789+Trapland@users.noreply.github.com>
2022-09-05 19:25:47 +0200

    Добавьте файлы проекта.

---
+++ Maze/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        Labirint l;
+        public int x_change = 0;
+        public int y_change = 0;
+        public int hp = 100;
+        public int medals = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Options();
+            StartGame();
+        }
+
+        public void Options()
+        {
+
+            BackColor = Color.FromArgb(255, 92, 118, 137);
+
+            int sizeX = 40;
+            int sizeY = 20;
+
+            Width = sizeX * 16 + 16;
+            Height = sizeY * 16 + 40;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        public void StartGame()
+        {
+            l = new Labirint(this, 40, 20);
+
+            this.Text = "Medals: " + medals.ToString() + "/" + l.totalmedals.ToString() + "  HP: " + hp.ToString() + "/100";
+            l.Show();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                x_change = -1;
+                y_change = 0;
+            }
+            else if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            {
+                x_change = 1;
+                y_change = 0;
+            }
+            else if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                x_change = 0;
+                y_change = -1;
+            }
+            else if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                x_change = 0;
+                y_change = 1;
+            }
+            else
+            {
+                x_change = 0;
+                y_change = 0;
+            }
+            if (l.maze[l.smileY + y_change, l.smileX + x_change].type == MazeObject.MazeObjectType.ENEMY)
+            {
+                hp -= 25;
+            }
+            if (l.maze[l.smileY + y_change, l.smileX + x_change].type == MazeObject.MazeObjectType.MEDAL)
+            {
+                medals++;
+            }
+            if (l.maze[l.smileY + y_change, l.smileX + x_change].type != l.maze[0, 0].type)
+            {
+                l.maze[l.smileY + y_change, l.smileX + x_change] = l.maze[l.smileY, l.smileX];
+                l.images[l.smileY + y_change, l.smileX + x_change].BackgroundImage = l.images[l.smileY, l.smileX].BackgroundImage;
+                l.maze[l.smileY, l.smileX] = new MazeObject(MazeObject.MazeObjectType.HALL);
+                l.images[l.smileY, l.smileX].BackgroundImage = l.maze[l.smileY, l.smileX].texture;
+                l.smileX += x_change;
+                l.smileY += y_change;
+            }
+            if (l.totalmedals == medals)
+            {
+                MessageBox.Show("You win by collecting medals!");
+                this.Close();
+            }
+            if (l.smileX == l.width - 1 && l.smileY == l.height - 3)
+            {
+                MessageBox.Show("You win!");
+                this.Close();
+            }
+            if (hp <= 0)
+            {
+                MessageBox.Show("You lose(");
+                this.Close();
+            }
+
+            this.Text = "Medals: " + medals.ToString() + "/" + l.totalmedals.ToString() + "  HP: " + hp.ToString() + "/100";
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+
+            l.Show();
+        }
+    }
+}
---
+++ Maze/Labirint.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Maze
+{
+    class Labirint
+    {
+        public int height; // высота лабиринта (количество строк)
+        public int width; // ширина лабиринта (количество столбцов в каждой строке)
+
+        public MazeObject[,] maze;
+        public PictureBox[,] images;
+
+        public static Random r = new Random();
+        public Form parent;
+
+        public int smileX = 0;
+        public int smileY = 2;
+        
+        public int totalmedals = 0;
+        public Labirint(Form parent, int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            this.parent = parent;
+
+            maze = new MazeObject[height, width];
+            images = new PictureBox[height, width];
+
+            Generate();
+        }
+
+        private void Generate()
+        {
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    MazeObject.MazeObjectType current = MazeObject.MazeObjectType.HALL;
+
+                    // наш персонажик
+                    if (x == smileX && y == smileY)
+                    {
+                        current = MazeObject.MazeObjectType.CHAR;
+                    }
+
+                    // стены по периметру обязательны
+                    else if (y == 0 || x == 0 || y == height - 1 | x == width - 1)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+                    // есть выход, и соседняя ячейка справа всегда свободна
+                    else if (x == smileX + 1 && y == smileY || x == width - 1 && y == height - 3)
+                    {
+                        current = MazeObject.MazeObjectType.HALL;
+                    }
+                    // в 1 случае из 5 - ставим стену
+                    else if (r.Next(5) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.WALL;
+                    }
+
+                    // в 1 случае из 100 - кладём денежку
+                    else if (r.Next(100) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.MEDAL;
+                        totalmedals++;
+                    }
+
+                    // в 1 случае из 100 - размещаем врага
+                    else if (r.Next(100) == 0)
+                    {
+                        current = MazeObject.MazeObjectType.ENEMY;
+                    }
+
+
+                    
+                    maze[y, x] = new MazeObject(current);
+                    images[y, x] = new PictureBox();
+                    images[y, x].Location = new Point(x * maze[y, x].width, y * maze[y, x].height);
+                    images[y, x].Parent = parent;
+                    images[y, x].Width = maze[y, x].width;
+                    images[y, x].Height = maze[y, x].height;
+                    images[y, x].BackgroundImage = maze[y, x].texture;
+                    images[y, x].Visible = false;
+                }
+            }
+        }
+
+        public void Show()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    images[y, x].Visible = true;
+                }
+            }
+        }
+
+        public void Update()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    images[y, x].Visible = false;
+                    images[y, x].Visible = true;
+                }
+            }
+        }
+
+    }
+}
